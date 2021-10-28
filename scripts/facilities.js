2b8bbdabe5533bcaa5f0978a09f9362fdaa18e6d
auntviv <aviva.mondesir@gmail.com>
2021-10-28 12:01:04 -0500

    Facilities Dropdown and Boolean

--- scripts/facilities.js
+++ scripts/facilities.js
@@ -1,22 +1,32 @@
 import { getFacilities } from "./database.js"
 
-const facilities = getFacilities() 
 
-export const Facilities = () => {
-    let html = "<ul>"
+document.addEventListener(
+    "change",
+    (changeEvent) => {
+        if (changeEvent.target.id === "facility") {
+            const facilityOption = changeEvent.target.value
+            console.log(facilityOption)  // "1" or "2"
+        }
+    }
+)
 
+
+export const Facilities = () => { // eporting the desired html to the page
+    const facilities = getFacilities() // creating a varible called facilitty. and assigning the value of an array "get fac"
+    //creating a varible called html and its assigned html to an empty string
+    let html= "" 
     
-    // This is how you have been converting objects to <li> elements
-//     for (const facility of facilities) {
-//         html += `<li>
-//             <<select id="resource">
-//             <option value="0">Choose a facility...${facility.id}" /> ${facility.name}</option>
-//             <option value="1">${facility.name}</option>
-//             <option value="2">Second option</option>
-//         </select>
-      
-//     `}
-
- 
-//     return html
-// }
+    html += ` <div> <label> Choose your facility </label> 
+    <select id="facility"> 
+    <option value="0">Choose your facility </option> 
+    ` //  21. adds the lable, 22. the select tag this code allows us the structure for the dropdown (provided by vs code) 23. is a place holder that prints "choose your facilities" before selection
+    for (const facility of facilities) { //iterating through each facility object in the facilities array 
+        if (facility.active === true) { // this condition will filter out all inactive gov.
+            html += `<option value="${facility.id}" > ${facility.name} </option>` //generates html that capures the names through the ids provided in the options
+        }
+        }
+    html+= "</select></div>"  //closing tags for HTML
+    
+ return html //returning the HTML string
+}
