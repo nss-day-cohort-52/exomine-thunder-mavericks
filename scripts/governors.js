7f081fac2bb473186ff4442f27fb42eb89d11075
Chelsea.harris <chelsea.harris@asurion.com>
2021-10-28 11:37:09 -0500

    drop down with boolean

--- scripts/governors.js
+++ scripts/governors.js
@@ -1,17 +1,34 @@
-import { getGovernors } from "./database.js"
+import { getGovernors } from "./database.js" /// import from datadase 
 
-const governors = getGovernors()
+/// we need a drop down to select active governers from the list
 
-export const Governors = () => {
-    let html = "<ul>"
+document.addEventListener(
+    "change",
+    (changeEvent) => {
+        if (changeEvent.target.id === "governor") {
+            const governerOption = changeEvent.target.value
+            console.log(governerOption)  // "1" or "2"
+        }
+    }
+)
 
+
+export const Governors = () => { // eporting the desired html to the page
+    const governors = getGovernors() // creating a varible called gov. and assigning the value of an array "get gov"
+    //creating a varible called html and its assigned html to an emty string
+    let html= "" 
     
-    // This is how you have been converting objects to <li> elements
-    for (const governor of governors) {
-        html += `<li>
-            <input type="radio" name="metal" value="${governor.id}" /> ${governor.name}
-    `}
-
-    html += "</ul>"
-    return html
+    html += ` <div> <label> Choose your governor </label> 
+    <select id="governor"> 
+    <option value="0">Choose your governor </option> 
+    ` //  21. adds the lable, 22. the select tag this code allows us the structure for the dropdown (provided by vs code) 23. is a place holder that prints "choose your govener" before selection
+    for (const governor of governors) { //inerating through each governer object in the governers array 
+        if (governor.active === true) { // this condition will filter out all inactive gov.
+            html += `<option value="${governor.id}" > ${governor.name} </option>` //generates html that capures the names through the ids provided in the options
+        }
+        }
+    html+= "</select></div>"  //closing tags for HTML
+    
+ return html //returning the HTML string
 }
+
